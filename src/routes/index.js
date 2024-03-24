3406be8e58e854016137fad2c973b92ac5a268f7
Natural <jarkurghan@gmail.com>
2024-03-24 17:47:19 +0500

    search API

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,6 +4,7 @@ import word from "./word/index.js";
 import resource from "./resource/index.js";
 import language from "./language/index.js";
 import file from "./file/index.js";
+import free from "./free/index.js";
 import auth from "../middleware/auth.js";
 const app = express();
 
@@ -12,5 +13,6 @@ app.use("/otil/v1/api/word", auth, word);
 app.use("/otil/v1/api/resource", resource);
 app.use("/otil/v1/api/language", auth, language);
 app.use("/otil/v1/api/file", file);
+app.use("/lugat", free);
 
 export default app;
