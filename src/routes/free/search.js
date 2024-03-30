a4958425236815155c298fa578a0c26a769d52ff
Natural <jarkurghan@gmail.com>
2024-03-30 18:12:07 +0500

    added promise to increase speed

--- src/routes/free/search.js
+++ src/routes/free/search.js
@@ -5,40 +5,75 @@ export async function search(req, res) {
         if (!req.query.page) req.query.page = 1;
         const search = req.query.request || "";
 
-        const words = await knex("words")
-            // .whereIn("words.status", [1, 2])
-            .leftJoin("definition", "definition.word", "words.id")
-            .select(["words.id", "words.word", "definition.definition"])
-            .whereLike("words.word", `%${search}%`);
+        const results = { words: [], defs: [], hiss: [], exas: [] };
+        const promises = [];
 
-        const defs = await knex("words")
-            // .whereIn("words.status", [1, 2])
-            .leftJoin("definition", "definition.word", "words.id")
-            .select(["words.id", "words.word", "definition.definition"])
-            .whereLike("definition.definition", `%${search}%`);
+        promises.push(
+            new Promise(async (resolve, reject) => {
+                try {
+                    results.words = await knex("words")
+                        // .whereIn("words.status", [1, 2])
+                        .leftJoin("definition", "definition.word", "words.id")
+                        .select(["words.id", "words.word", "definition.definition"])
+                        .whereLike("words.word", `%${search}%`);
+                    resolve();
+                } catch (error) {
+                    reject(error);
+                }
+            })
+        );
 
-        const hiss = await knex("words")
-            // .whereIn("words.status", [1, 2])
-            .leftJoin("history", "history.word", "words.id")
-            .select(["words.id", "words.word", "history.history", "history.resource"])
-            .whereLike("history.history", `%${search}%`);
+        promises.push(
+            new Promise(async (resolve, reject) => {
+                try {
+                    results.defs = await knex("words")
+                        // .whereIn("words.status", [1, 2])
+                        .leftJoin("definition", "definition.word", "words.id")
+                        .select(["words.id", "words.word", "definition.definition"])
+                        .whereLike("definition.definition", `%${search}%`);
+                    resolve();
+                } catch (error) {
+                    reject(error);
+                }
+            })
+        );
 
-        const exas = await knex("words")
-            // .whereIn("words.status", [1, 2])
-            .leftJoin("example", "example.word", "words.id")
-            .select(["words.id", "words.word", "example.phrase", "example.resource"])
-            .whereLike("example.phrase", `%${search}%`);
+        promises.push(
+            new Promise(async (resolve, reject) => {
+                try {
+                    results.hiss = await knex("words")
+                        // .whereIn("words.status", [1, 2])
+                        .leftJoin("history", "history.word", "words.id")
+                        .select(["words.id", "words.word", "history.history", "history.resource"])
+                        .whereLike("history.history", `%${search}%`);
+                    resolve();
+                } catch (error) {
+                    reject(error);
+                }
+            })
+        );
 
-        // const sins = await knex("words")
-        //     .where("words.status", 4)
-        //     .leftJoin("example", "example.word", "words.id")
-        //     .select(["words.id", "words.word", "example.phrase", "example.resource"])
-        //     .whereLike("example.phrase", `%${search}%`);
+        promises.push(
+            new Promise(async (resolve, reject) => {
+                try {
+                    results.exas = await knex("words")
+                        // .whereIn("words.status", [1, 2])
+                        .leftJoin("example", "example.word", "words.id")
+                        .select(["words.id", "words.word", "example.phrase", "example.resource"])
+                        .whereLike("example.phrase", `%${search}%`);
+                    resolve();
+                } catch (error) {
+                    reject(error);
+                }
+            })
+        );
 
-        const data = [...words];
-        data.push(...defs.filter((e) => !data.find((i) => e.id === i.id)));
-        data.push(...hiss.filter((e) => !data.find((i) => e.id === i.id)));
-        data.push(...exas.filter((e) => !data.find((i) => e.id === i.id)));
+        await Promise.all(promises);
+
+        const data = [...results.words];
+        data.push(...results.defs.filter((e) => !data.find((i) => e.id === i.id)));
+        data.push(...results.hiss.filter((e) => !data.find((i) => e.id === i.id)));
+        data.push(...results.exas.filter((e) => !data.find((i) => e.id === i.id)));
 
         setTimeout(() => {
             res.status(200).json(data);
