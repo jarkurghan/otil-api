35b90d4b0cc5289be09a2afe606df06fa04a7b68
Najmiddin Nazirov <nnazirov@transoxiania.com>
2024-08-29 11:35:12 +0500

    new commit

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 import express from "express";
-import bodyParser from "body-parser";
-import cors from "cors";
-import helmet from "helmet";
-import morgan from "morgan";
+// import bodyParser from "body-parser";
+// import cors from "cors";
+// import helmet from "helmet";
+// import morgan from "morgan";
 // import router from "./src/routes/index.js";
 // import env from "./src/environment/_env_module.js";
 const app = express();
 // env();
 
-app.use(cors());
-app.use(helmet());
-app.use(morgan("combined"));
-app.use(bodyParser.json());
+// app.use(cors());
+// app.use(helmet());
+// app.use(morgan("combined"));
+// app.use(bodyParser.json());
 
 // import swaggerJSDoc from "swagger-jsdoc";
 // import swaggerUi from "swagger-ui-express";
@@ -20,5 +20,5 @@ app.use(bodyParser.json());
 
 // app.use("/", router);
 app.listen(process.env.PORT, () => {
-    console.log(`Ilova ishga tushdi: http://localhost:${process.env.PORT}`);
+    console.log(`Ilova ishga tushdi: http://localhost:${process.env.PORT || 2006}`);
 });
