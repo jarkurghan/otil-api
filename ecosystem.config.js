91f9daae4d92e73502629110088fe580faabe274
Najmiddin Nazirov <nnazirov@transoxiania.com>
2024-08-29 12:58:37 +0500

    new commit

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,10 +1,8 @@
-module.exports = {
-    apps: [
-      {
+export const apps = [
+    {
         name: 'api',
         script: './index.js',
         interpreter: 'node@18', // Yoki sizning Node.js versiyangizga mos keladigan versiya
-      },
-    ],
-  };
+    },
+];
   
