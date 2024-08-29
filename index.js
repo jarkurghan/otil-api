41fd100b9d0e65da8dd99b922890405000aa421b
Najmiddin Nazirov <nnazirov@transoxiania.com>
2024-08-29 11:33:43 +0500

    new commit

--- index.js
+++ index.js
@@ -3,22 +3,22 @@ import bodyParser from "body-parser";
 import cors from "cors";
 import helmet from "helmet";
 import morgan from "morgan";
-import router from "./src/routes/index.js";
-import env from "./src/environment/_env_module.js";
+// import router from "./src/routes/index.js";
+// import env from "./src/environment/_env_module.js";
 const app = express();
-env();
+// env();
 
 app.use(cors());
 app.use(helmet());
 app.use(morgan("combined"));
 app.use(bodyParser.json());
 
-import swaggerJSDoc from "swagger-jsdoc";
-import swaggerUi from "swagger-ui-express";
-import options1 from "./src/swagger/swagger_options.js";
-app.use("/swagger", swaggerUi.serve, swaggerUi.setup(swaggerJSDoc(options1)));
+// import swaggerJSDoc from "swagger-jsdoc";
+// import swaggerUi from "swagger-ui-express";
+// import options1 from "./src/swagger/swagger_options.js";
+// app.use("/swagger", swaggerUi.serve, swaggerUi.setup(swaggerJSDoc(options1)));
 
-app.use("/", router);
+// app.use("/", router);
 app.listen(process.env.PORT, () => {
     console.log(`Ilova ishga tushdi: http://localhost:${process.env.PORT}`);
 });
--- src/tools/send-message.js
+++ src/tools/send-message.js
@@ -4,20 +4,8 @@ export async function sendSMS({ text }) {
     const x = await axios({
         method: "post",
         url: "https://mmlyk6.api.infobip.com/sms/2/text/advanced",
-        headers: {
-            Authorization: process.env.INFOBIPTOKEN,
-            "Content-Type": "application/json",
-            Accept: "application/json",
-        },
-        data: {
-            messages: [
-                {
-                    destinations: [{ to: process.env.INFOBIPNUMBER }],
-                    from: "Xabarnoma",
-                    text,
-                },
-            ],
-        },
+        headers: { Authorization: process.env.INFOBIPTOKEN, "Content-Type": "application/json", Accept: "application/json" },
+        data: { messages: [{ destinations: [{ to: process.env.INFOBIPNUMBER }], from: "Xabarnoma", text }] },
     });
     console.log(x.data);
     return 0;
